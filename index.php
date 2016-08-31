c1eccbe8e33dedc364b5b4b5adb57066fe0a23fa
scouse <notescouse@gmail.com>
2016-08-31 17:47:30 +0700

    meta viewport

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta http-equiv="Content-Style-Type" content="text/css" />
 <meta http-equiv="Content-Script-Type" content="text/javascript" />
+<meta name="description" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <title>DeOne</title>
 <meta name="keywords" content="" />
 <meta name="description" content="" />
